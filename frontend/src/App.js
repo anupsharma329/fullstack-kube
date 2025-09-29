e95221c0d0fa63b1fbd0c551ed25602bb9ec08f3
anupsharma329 <anupsharma329@gmail.com>
2025-09-30 01:36:21 +0530

    changes in frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
       .then((json) => setData(json.message))
       .catch((err) => {
         console.error("API Error:", err);
-        setData("this is anupsharma frontend");
+        setData("This is created by anupsharma frontend");
       });
   }, []);
 
